95ee5627c45d6c4ee332208954e652f4c6e0c5a5
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-09-24 01:13:43 +0300

    Exercise№1 completed

---
+++ Exercise№1/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = GetNumber("Enter your three-digit number ");
+CutDigits(number);
+
+void CutDigits(int a)
+{
+    int secondDigit = 0;
+    if (a > 99 && a < 1000)
+    {
+        Console.WriteLine(secondDigit = (a % 100) / 10);
+    }
+    else
+    {
+        Console.WriteLine("Please enter a three-digit number!");
+    }
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
