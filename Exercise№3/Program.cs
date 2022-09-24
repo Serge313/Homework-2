adccb1b928ef6c91505a6c8d77bd8264c7ccc8c1
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-09-24 04:41:01 +0300

    Exercise№3 completed

---
+++ Exercise№3/Program.cs
@@ -0,0 +1,25 @@
+﻿int digit = GetDigit("Enter your digit from 1 to 7 ");
+WeekendsOrNot(digit);
+
+void WeekendsOrNot (int a)
+{
+    if (a > 5 && a < 8)
+    {
+        Console.WriteLine($"{a} -> yes");
+    }
+    else if (a >=1 && a <= 5)
+    {
+        Console.WriteLine($"{a} -> no");
+    }
+    else
+    {
+        Console.WriteLine($"Your digit - \"{a}\" is more then 7 or less then 1!");
+    }
+}
+
+
+int GetDigit(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
