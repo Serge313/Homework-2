08cc6846c0728a611ac1d7c5b3071cd6998b8722
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-09-24 04:27:58 +0300

    Exercise№2 completed

---
+++ Exercise№2/Program.cs
@@ -0,0 +1,54 @@
+﻿int number = GetNumber("Enter your number ");
+int count = CountDigits(number);
+int[] array = new int [count];
+FillArrayAndFindThirdDigit(array, number);
+
+//Использую функцию PrintArray для проверки заполнения массива
+void PrintArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write(array1[i]);
+    }
+}
+
+
+void FillArrayAndFindThirdDigit(int[] array1, int a)
+{
+    if (a > 99)
+    {
+        for (int i = array1.Length - 1; i >= 0; i--)
+        {
+            int cutDigit = a % 10;
+            array[i] = cutDigit;
+            a = a / 10;
+        }
+        Console.WriteLine(array1[2]);
+        PrintArray(array);
+    }
+    else
+    {
+        Console.WriteLine("There is no third digit!");
+    }
+}
+
+
+int CountDigits(int a)
+{
+    int count = 0;
+    int rest = a;
+    while (a != 0)
+    {
+        rest = a / 10;
+        a = a / 10;
+        count ++;
+    }
+    return count;
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
